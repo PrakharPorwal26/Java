84aec383d1affba30bcd55472a03a0b019a47179
Prakhar Porwal <prakhar2001porwal@gmail.com>
2026-07-01 17:48:06 +0530

    Inner Class

--- NestedClasses/InnerClass.java
+++ NestedClasses/InnerClass.java
@@ -15,9 +15,19 @@ public class InnerClass {
 }
 
 class Outer{
+
+    int x = 10;
+
     class Inner{
+        int x = 20;
         void display(){
+            
             System.out.println("This is an inner class");
+
+            System.out.println(x); //prints 20 because inner class has access to outer class's members, but if inner class has a member with the same name as outer class, then inner class's member will be used. This is called variable shadowing.
+
+            System.out.println(Outer.this.x); //prints 10 because we are using Outer.this to access outer class's member variable x.
+
         }
         void show(){
             System.out.println("This is a method in the inner class");
