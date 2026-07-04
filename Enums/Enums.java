ac1279ef3ea8d2a4498ac3e3b2313e16ece61de1
Prakhar Porwal <prakhar2001porwal@gmail.com>
2026-07-04 14:22:21 +0530

    Syntax of Enum, assigning them values and showing polymorphism using anonymous class

--- Enums/Enums.java
+++ Enums/Enums.java
@@ -5,25 +5,48 @@ public class Enums{
         Direction d = Direction.NORTH;
         System.out.println(d); //prints NORTH
         System.out.println(d.getDegrees()); //prints 0
+        d.move();
     }
 }
 
-/*          NORTH(0)
-                |
-                |
-WEST(270)<------|------->EAST(90)
-                |
-                |
-            SOUTH(180)
+/*               NORTH(0, y+1)
+                     |
+                     |
+WEST(270, x-1)<------|------->EAST(90, x+1)
+                     |
+                     |
+                SOUTH(180, y-1)
 */
 
 
-//Syntax of enum along with assigning values to enum constants
+//Syntax of enum along with assigning values to enum constants along with a method which shows different behaviours for different enum constants --> abstract method can be used to achieve this. 
 enum Direction{
-    NORTH(0),
-    SOUTH(180),
-    WEST(270),
-    EAST(90);
+
+    //to implement abstract method, we need to override it in each enum constant, just like anonymous class.
+    NORTH(0){
+        @Override
+        public void move() {
+            System.out.println("Moving North, y+1");
+        }
+    },
+    SOUTH(180){
+        @Override
+        public void move() {
+            System.out.println("Moving South, y-1");
+        }
+    },
+    WEST(270){
+        @Override
+        public void move() {
+            System.out.println("Moving West, x-1");
+        }
+    },
+    EAST(90){
+        @Override
+        public void move() {
+            System.out.println("Moving East, x+1");
+        }
+    };
 
     private int degrees;
 
@@ -34,6 +57,9 @@ enum Direction{
     public int getDegrees(){
         return degrees;
     }
+    
+    //abstract method
+    public abstract void move();
 }
 
 /*
