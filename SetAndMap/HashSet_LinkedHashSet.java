75dd92c8b486c22a51ed975f33a345b0f722f696
Prakhar Porwal <prakhar2001porwal@gmail.com>
2026-08-15 16:18:00 +0530

    added comments

--- SetAndMap/HashSet_LinkedHashSet.java
+++ SetAndMap/HashSet_LinkedHashSet.java
@@ -1,5 +1,5 @@
 package SetAndMap;
-import java.util.*;
+// import java.util.*;
 class HashSet_LinkedHashSet{
     public static void main(String[] args) {
         /*----------HashSet---------- */
@@ -7,15 +7,15 @@ class HashSet_LinkedHashSet{
         //Constructors of HashSet (Same for LinkedHashSet, replace HashSet->LinkedHashSet)
 
         //1: Empty HashSet with standard capacity of 16
-        Set<Integer> s1 = new HashSet<>();
+        // Set<Integer> s1 = new HashSet<>();
 
         //2. HashSet with some initial capacity
-        Set<Integer> s2 = new HashSet<>(100);
+        // Set<Integer> s2 = new HashSet<>(100);
 
         //3. HashSet with initial capacity and load factor
-        Set<Integer> s3 = new HashSet<>(100,0.8f);
+        // Set<Integer> s3 = new HashSet<>(100,0.8f);
 
         //4. Using another collection
-        Set<Integer> s4 = new HashSet<>(List.of(1,2,3,4,5,6,7,8));
+        // Set<Integer> s4 = new HashSet<>(List.of(1,2,3,4,5,6,7,8));
     }
 }
