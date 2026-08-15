712ac6678bb12e2841edd37d2e6bfbd3b7d64e45
Prakhar Porwal <prakhar2001porwal@gmail.com>
2026-08-15 16:31:22 +0530

    Constructors of Hash Map and Linked Hash Map

---
+++ SetAndMap/HashMap_LinkedHashMap.java
@@ -0,0 +1,12 @@
+package SetAndMap;
+// import java.util.*;
+public class HashMap_LinkedHashMap {
+    public static void main(String[] args) {
+        //Constructors of HashMap and LinkedHashMap(replace HashMap->LinkedHashMap)
+        // Map<Integer, String> mp = new HashMap<>();
+        // Map<Integer, String> mp1 = new HashMap<>(100);
+        // Map<Integer, String> mp2 = new HashMap<>(100,0.8f);
+        // Map<Integer, String> mp3 = Map.of(101,"Prakhar", 102,"Tushar");
+        // Map<Integer, String> mp4 = new HashMap<>(mp3);
+    }
+}
